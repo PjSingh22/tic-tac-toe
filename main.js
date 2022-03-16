7af04856d7814a9ebb673bdd4e2932b0a7706e06
Prabhjot Singh <pjsingh1359@gmail.com>
2022-03-15 19:41:16 -0700

    add styles

--- main.js
+++ main.js
@@ -5,21 +5,10 @@ const gameBoard = (() => {
   let boardArr = ["x", "x", "x", "o", "o", "o", "x", "x", "x"];
   
   const displayBoard = () => { 
-    // boardArr.map((i, idx) => {
-    //   const boardSqare = `<div class="board-square" data-index="${idx}">${i}</div>`;
-    //   boardContainer.innerHTML += boardSqare;
-    // });
-
-    for (let i = 0; i < boardArr.length; i++) {
-      // const boardSquare = document.createElement("div");
-      // boardSquare.classList.add("board-square");
-      // boardSquare.setAttribute("id", i);
-      // boardSquare.textContent = boardArr[i];
-      // boardContainer.appendChild(boardSquare);
-
-      const boardSqare = `<div class="board-square" data-index="${i}">${boardArr[i]}</div>`;
+    boardArr.map((i, idx) => {
+      const boardSqare = `<div class="board-square" data-index="${idx}">${i}</div>`;
       boardContainer.innerHTML += boardSqare;
-    }
+    });
   };
 
   return {
